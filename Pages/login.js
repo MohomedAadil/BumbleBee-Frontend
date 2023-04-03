e558b932e099af9acd15d558639e23a97880b7ec
MohomedAadil <aadil.mohomed786@gmail.com>
2023-04-03 06:45:21 +0530

    admin dashboard baics

---
+++ Pages/login.js
@@ -0,0 +1,27 @@
+function handleLogin(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const userData = {
+      email: formData.get('email'),
+      password: formData.get('password'),
+    };
+    fetch('http://localhost:9191/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userData)
+    })
+    .then(response => {
+      if (response.status === 200) {
+        window.location.href = 'index.html';
+        alert("Admin Login Successful!");
+      } else {
+        alert("Invalid Login! Please Try again.");
+      }
+    })
+    .catch(error => {
+      console.error('Fetch operation error:', error);
+      // handle error
+    });
+  }
