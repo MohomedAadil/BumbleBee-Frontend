b2203d89cc26a3781390398e093851ff2b3ae2fa
MohomedAadil <aadil.mohomed786@gmail.com>
2023-04-04 12:13:40 +0530

    admin changes

--- Pages/login.js
+++ Pages/login.js
@@ -1,27 +1,50 @@
-function handleLogin(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const userData = {
-      email: formData.get('email'),
-      password: formData.get('password'),
-    };
-    fetch('http://localhost:9191/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(userData)
-    })
-    .then(response => {
-      if (response.status === 200) {
-        window.location.href = 'index.html';
-        alert("Admin Login Successful!");
-      } else {
-        alert("Invalid Login! Please Try again.");
-      }
-    })
-    .catch(error => {
-      console.error('Fetch operation error:', error);
-      // handle error
-    });
-  }
+// function handleLogin(event) {
+//     event.preventDefault();
+
+//     const formData = new FormData(event.target);
+//     const userData = {
+//       username: formData.get('username'),
+//       password: formData.get('password'),
+//     };
+//     fetch('http://localhost:9191/login', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(userData)
+//     })
+//     .then(response => {
+//       if (response.status === 200) {
+//         window.location.href = 'index.html';
+//         alert("Admin Login Successful!");
+//       } else {
+//         alert("Invalid Login! Please Try again.");
+//       }
+//     })
+//     .catch(error => {
+//       console.error('Fetch operation error:', error);
+//       // handle error
+//     });
+//   }
+
+
+function login(username, password) {
+  fetch('http://localhost:9191/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ username, password })
+  })
+  .then(response => {
+    if (response.ok) {
+      alert('Login successful!');
+      window.location.href = 'index.html';
+    } else {
+      alert('Login failed!');
+    }
+  })
+  .catch(error => {
+    alert('Error: ' + error);
+  });
+}
